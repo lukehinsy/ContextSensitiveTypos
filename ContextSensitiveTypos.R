4ed3bc9d086e4d50b4e39757906ede47da928ed5
lukehinsy <hinsenkamp@gmail.com>
2021-02-09 18:34:58 -0500

    Creating / Initial commit.
    Should work as is:
     Feed column of strings.
     Unnest those
     Hunspell to look for nonwords
     Use word distribution of correct words to find best replacement
     Use original non-word if no good replacement.
     Return df with misspellings and suggestions.

---
+++ ContextSensitiveTypos.R
@@ -0,0 +1,19 @@
+library(dplyr)
+
+wordlist<-text %>%
+  mutate(text=tm::removePunctuation(text,preserve_intra_word_contractions=TRUE)) %>%
+  tidytext::unnest_tokens(word, text, token="words",to_lower=TRUE, strip_punct=FALSE, strip_numeric=TRUE) %>%
+  dplyr::count(word, sort=TRUE) %>%
+  select(word)
+
+wordlist$correct<-hunspell::hunspell_check(toupper(wordlist$word))
+correctwords<-wordlist %>% filter(correct=TRUE)
+misspellings<-wordlist %>% filter(correct=FALSE)
+
+correct <-function(word) {c(correctwords[adist(word, correctwords$word) <= min(adist(word, correctwords$word), 2)], word)[1]}
+
+misspellings$sugg<-unlist(lapply(misspellings$word, correct))    ## THIS IS INCREDIBLY SLOW!
+misspellings <- misspelings %>% select(word, sugg)
+return(misspellings)
+  ## Currently returning just the typos and top suggestion. Can use that df as a dictionary to grepl fix. 
+    # Might, however, want to change this behavior.
