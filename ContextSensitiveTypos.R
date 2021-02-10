0a516370a6fa40e80e422fde5cdbfaa271b5dbce
Luke Hinsenkamp <hinsel1@nationwide.com>
2021-02-10 09:51:30 -0500

    IT  WORKS!
    Fully operational function.
    Currently just returning the 2-column word/sugg output; in future, add arg to do taht correction and spit back out full input DF.
    VERY slow due to the lapply, though.

--- ContextSensitiveTypos.R
+++ ContextSensitiveTypos.R
@@ -6,23 +6,27 @@ FixTypos<-function(df, text) {
   require(tm)
   require(tidytext)
   require(hunspell)
-  
-wordlist<-df %>% select(text) %>%
-  dplyr::mutate(text=tm::removePunctuation(text,preserve_intra_word_contractions=TRUE)) %>%
+	
+#   df<-cemdata.trim
+# 	df<-df %>% rename(text=q1a)
+ 
+	wordlist<-df %>% select({{text}}) %>%
+  dplyr::mutate(text=tm::removePunctuation({{text}},preserve_intra_word_contractions=TRUE)) %>%
   tidytext::unnest_tokens(word, text, token="words",to_lower=TRUE, strip_punct=FALSE, strip_numeric=TRUE) %>%
   dplyr::count(word, sort=TRUE) %>%
   dplyr::select(word)
 
 wordlist$correct<-hunspell::hunspell_check(toupper(wordlist$word))
-correctwords<-wordlist %>% filter(correct=TRUE)
-misspellings<-wordlist %>% filter(correct=FALSE)
+correctwords<-wordlist %>% filter(correct==TRUE)
+misspellings<-wordlist %>% filter(correct==FALSE)
 
 correct <-function(word) {c(correctwords[adist(word, correctwords$word) <= min(adist(word, correctwords$word), 2)], word)[1]}
 
 misspellings$sugg<-unlist(lapply(misspellings$word, correct))    ## THIS IS INCREDIBLY SLOW!
-misspellings <- misspelings %>% select(word, sugg)
+misspellings <- misspellings %>% select(word, sugg)
 return(misspellings)
   ## Currently returning just the typos and top suggestion. Can use that df as a dictionary to grepl fix. 
     # Might, however, want to change this behavior.remote
 }
 
+# test<-FixTypos(cemdata.trim,q1a)
